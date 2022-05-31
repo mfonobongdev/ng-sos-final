b9038a76c1a0e5499b29fa3bc2ba5ea741e176e3
mfonobongdev <93042878+mfonobongdev@users.noreply.github.com>
2022-05-31 14:47:13 +0100

    added image responsiveness fix

--- page-components/Home/DownloadSection.tsx
+++ page-components/Home/DownloadSection.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import heroImageTwo from '/public/assets/mobile-front-right.png'
+import heroImageTwo from '/public/assets/mobile-front-stack.png'
 import Spacer from '../../components/primitives/Spacer'
 import { AppStoreButton, PlayStoreButton } from '../../components/primitives/StoreButtons'
 
@@ -8,13 +8,8 @@ export const DownloadSection = (): JSX.Element => {
     <div className='bg-dullOrange'>
       <section className='container mx-auto py-6 md:py-24'>
         <div className='grid grid-rows-[max-content_1fr] grid-cols-1 md:grid-rows-1 md:grid-cols-[1fr_2fr] gap-10 md:gap-3'>
-          <div className='relative w-full h-[600px] md:h-[680px] rounded-sm order-last md:order-first mx-12 md:mx-0'>
-            <Image
-              src={heroImageTwo}
-              alt={'hero'}
-              layout={'fill'}
-              className={'object-contain object-left md:object-center rounded-sm z-0'}
-            />
+          <div className='relative w-full h-[600px] md:h-[680px] rounded-sm order-last md:order-first'>
+            <Image src={heroImageTwo} alt={'hero'} layout={'fill'} className={'object-contain object-center rounded-sm z-0'} />
           </div>
           <div className='md:place-self-center px-12'>
             <div className=''>
--- page-components/Home/HeroSection.tsx
+++ page-components/Home/HeroSection.tsx
@@ -9,16 +9,11 @@ export const HeroSection = (): JSX.Element => {
     <div className='bg-dullOrange'>
       <section className='container mt-10 mx-auto py-16 md:py-24'>
         <div className='grid grid-rows-[max-content_1fr] grid-cols-1 md:grid-rows-1 md:grid-cols-[1fr_2fr] gap-10 md:gap-3'>
-          <div className='hidden md:block relative w-full h-[600px] md:h-[680px] rounded-sm order-last md:order-first mx-12 md:mx-0'>
+          <div className='hidden md:block relative w-full h-[600px] md:h-[680px] rounded-sm order-last md:order-first'>
             <Image src={heroImage} alt={'hero'} layout={'fill'} className={'object-contain object-left md:object-center rounded-sm z-0'} />
           </div>
-          <div className='block md:hidden relative w-full h-[600px] md:h-[680px] rounded-sm order-last md:order-first mx-12 md:mx-0'>
-            <Image
-              src={heroImageTwo}
-              alt={'hero'}
-              layout={'fill'}
-              className={'object-contain object-left md:object-center rounded-sm z-0'}
-            />
+          <div className='block md:hidden relative w-full h-[600px] md:h-[680px] rounded-sm order-last md:order-first'>
+            <Image src={heroImageTwo} alt={'hero'} layout={'fill'} className={'object-contain object-center rounded-sm z-0'} />
           </div>
           <div className='md:place-self-center px-12'>
             <div className=''>
