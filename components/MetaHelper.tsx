45bca490a2f0d6f69a3ef2bf39398c960ada0c53
mfonobongdev <93042878+mfonobongdev@users.noreply.github.com>
2022-06-20 08:46:05 +0100

    updated logo

--- components/MetaHelper.tsx
+++ components/MetaHelper.tsx
@@ -4,7 +4,7 @@ export const MetaHelper = ({ title }: { title: string }): JSX.Element => {
   const content = `NG-SOS provides a full-service emergency assistance platform by alerting the closest private security, and medical response provider in your immediate vicinity, anywhere in Nigeria.`
   return (
     <Head>
-      <title>{title} - NGSOS</title>
+      <title>{title} - NIGSOS</title>
       <meta name='description' content={content} />
       <link rel='icon' href='/favicon.ico' />
     </Head>
