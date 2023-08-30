634eae65d7b306b7ba610a64675b2fe9cf6a3284
mfonobongdev <93042878+mfonobongdev@users.noreply.github.com>
2023-08-30 15:54:27 +0100

    added new image for control room page

--- page-components/Control/HeaderSection.tsx
+++ page-components/Control/HeaderSection.tsx
@@ -1,19 +1,18 @@
 import Image from 'next/image'
-import heroImage from '/public/assets/mobile-front.png'
-import heroImageTwo from '/public/assets/mobile-front-right.png'
+import heroImage from '/public/assets/control-hero.jpg'
 import Spacer from '../../components/primitives/Spacer'
-import { AppStoreButton, PlayStoreButton } from '../../components/primitives/StoreButtons'
+
 
 export const HeaderSection = (): JSX.Element => {
   return (
-    <div className='bg-red-50'>
-      <section className='container mx-auto py-6 md:py-24'>
+    <div className='relative'>
+      <section className='container mx-auto py-6 md:py-24 z-20'>
         <div className='grid pt-20'>
           <div className='md:place-self-center px-12'>
             <div className=''>
-              <h1 className='font-Poppins font-bold text-3xl lg:text-5xl text-primary leading-normal max-w-[20ch]'>Control Room.</h1>
+              <h1 className='font-Poppins font-bold text-3xl lg:text-5xl text-white leading-normal max-w-[20ch]'>Control Room.</h1>
               <Spacer />
-              <h3 className='font-Poppins font-normal text-base lg:text-lg text-secondary leading-normal max-w-[60ch]'>
+              <h3 className='font-Poppins font-normal text-base lg:text-lg text-white leading-normal max-w-[60ch]'>
                 In order to ensure efficient and effective call taking and call handling, we use one national number operated through a
                 high-tech emergency contact centre twenty seven hours per day, everyday. The call centre is supported by an independent
                 off-site disaster and back up location.
@@ -23,6 +22,13 @@ export const HeaderSection = (): JSX.Element => {
           </div>
         </div>
       </section>
+      <div className={'absolute inset-0 bg-gray-700/60 -z-10'} />
+      <div className={'absolute inset-0 bg-sosRed/40 -z-10'} />
+      <div className={'absolute inset-0 -z-20'}>
+        <div className={'relative h-full w-full'}>
+          <Image src={heroImage} alt={'hero'} layout={'fill'} className={'object-cover object-center -z-20'} />
+        </div>
+      </div>
     </div>
   )
 }
