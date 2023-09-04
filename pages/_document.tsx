d797bd0cc1655adc03093e389f8b6014b922169c
mfonobongdev <93042878+mfonobongdev@users.noreply.github.com>
2023-09-04 19:10:01 +0100

    updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,25 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
 import React from 'react'
 
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
-  ): Promise<{ head?: Array<JSX.Element | null>; html: string; styles?: React.ReactElement[] | React.ReactFragment }> {
+  ): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage
+ 
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      })
+ 
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+ 
+    return initialProps
   }
 
   render(): JSX.Element {
